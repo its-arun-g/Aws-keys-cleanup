2ca9052dde19f7f5e14c375e550a57497ae00b03
Arun240726 <mkak2407@gmail.com>
2025-02-13 17:51:18 +0530

    added user groups removal

--- src/user_manager.py
+++ src/user_manager.py
@@ -7,6 +7,12 @@ logger = get_logger()
 def delete_user(username):
     """Delete a single IAM user."""
     try:
+        # Detach user from groups
+        groups = iam_client.list_groups_for_user(UserName=username)
+        for group in groups['Groups']:
+            iam_client.remove_user_from_group(UserName=username, GroupName=group['GroupName'])
+            #logger.info(f"Removed {username} from group {group['GroupName']}")
+
         # Delete all access keys for the user
         response = iam_client.list_access_keys(UserName=username)
         for key in response.get('AccessKeyMetadata', []):
